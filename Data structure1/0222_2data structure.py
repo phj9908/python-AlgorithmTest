5c3d6449943b121aed3933acc781a10a51a9dcb8
HyeJi <phj9908@gmail.com>
2022-02-22 23:09:46 +0900

    datastructure

---
+++ Data structure1/0222_2data structure.py
@@ -0,0 +1,24 @@
+import sys
+word = list(sys.stdin.readline().rstrip())
+
+idx=0 # 현재 인덱스
+start=0 # 태그 제외 숫자나 알파벳 시작 인덱스
+
+while idx < len(word):
+    if word[idx] == '<':
+        idx+=1
+        while word[idx] !='>':
+            idx+=1
+        idx+=1
+    elif word[idx].isalnum() : # 숫자나 알파벳이면
+        start = idx
+        while idx < len(word) and word[idx].isalnum():
+            idx+=1
+        reverse_word = word[start:idx] # 태그 외에 문자열들 
+        reverse_word.reverse()
+        word[start:idx]=reverse_word # 다시 넣기
+    else : # 괄호,숫자, 알파벳이 아닌 공백일 때
+        idx +=1
+
+print(''.join(word))
+
