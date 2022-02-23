a45bd34b715bd0e7eac854be93a3ea993ecf34de
HyeJi <phj9908@gmail.com>
2022-02-24 00:13:03 +0900

    stack

---
+++ Data structure1/0223_2stack.py
@@ -0,0 +1,19 @@
+# 1.자신보다 인덱스가 클것  2. 자신보다 등장횟수가 더 큰 수 일 것 3. 복수 있는경우 인덱스가 가장 작은 것
+# Counter클래스 이용
+# dictionary 구조로 각 원소의 진도수 도출 
+# Counter('hello') : Counter({'h':1,'e':1,'l':2,'o':1})
+
+from collections import Counter 
+import sys
+
+n = int(sys.stdin.readline())
+nums = list(map(int, sys.stdin.readline().split())) 
+nums_cnt = Counter(nums) # Counter 클래스 객체 생성
+stack = [] # nums인덱스 할당용
+result=[-1]*n
+
+for i in range(n):
+    while stack and (nums_cnt[nums[stack[-1]]] < nums_cnt[nums[i]]): # 자신보다 더 큰 빈도 값이 나타났을 때
+        result[stack.pop()] = nums[i]
+    stack.append(i)
+print(*result) # -1 -1 1 2 2 1 -1
