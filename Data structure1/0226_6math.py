d6c5cd383a3b27061a6c1c022b00e9380f15ef8d
HyeJi <phj9908@gmail.com>
2022-02-26 23:48:13 +0900

    math

---
+++ Data structure1/0226_6math.py
@@ -0,0 +1,33 @@
+# 9613 : gcd합(최대공약수 누적합)
+
+from itertools import combinations
+
+
+n=int(input())
+
+def gcd(a,b): # 최대공약수 구하기
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+
+for i in range(n):
+    arr = list(map(int,input().split()))
+    sum=0
+    for j in range(1,len(arr)-1): # 맨앞의 수는 입력할 수의 갯수임
+        for k in range(j+1,len(arr)):
+            sum +=gcd(arr[j],arr[k])
+    print(sum)
+
+# for j in range(1,len(arr)-1): # 맨앞의 수는 입력할 수의 갯수임
+#         for k in range(j+1,len(arr)):
+#             sum +=gcd(arr[j],arr[k])
+#
+# 위의 이중 for문 말고도 combinations(리스트,짝지을 구성원 수)으로도 가능
+# ex) list=[1.2.3] , combinations(list,2) = [(1,2),(2,3)] , ( (1,2)와 (2,1)를 하나로 취급 )
+#
+# for j in list(combinations(arr[1:],2)):  
+#     if j[0] > j[1]:
+#         sum +=gcd(j[0],j[1])
+#     else:
+#         sum +=gcd(j[1],j[0])
---
+++ Data structure1/0226_7math.py
@@ -0,0 +1,20 @@
+#17087 숨바꼭질6 : 유클리드 호제법 활용
+# 수빈이의 위치와 각 동생들이 위치한 지점까지의 거리들 사이 최대공약수 구하기
+n,s = map(int,input().split())
+a=list(map(int,input().split()))
+res=0
+
+def gcd(a,b):
+    if b==0:
+        return a
+    else :
+        return gcd(b,a%b)
+
+dif=[]
+for i in range(n): # 각 동생들과 수빈이 사이 거리 도출
+    dif.append(abs(a[i]-s)) 
+b=dif[0]
+for i in range(1,n): # 첫번째 수와 두번째 수의 최대공약수 구한 뒤, 그 최대공약수와 세번째 수 의 최대공약수 구하고...마지막까지 반복
+    b= gcd(dif[i],b)
+
+print(b)
