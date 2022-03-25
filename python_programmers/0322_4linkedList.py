281cbb25c1b1bcd6047983511a6bf2d528335968
HyeJi <phj9908@gmail.com>
2022-03-25 22:26:32 +0900

    0325

--- python_programmers/0322_4linkedList.py
+++ python_programmers/0322_4linkedList.py
@@ -1,6 +1,5 @@
-# 링크드 리스트 순회 구현하기
-# traverse(),popAt() 구현
-# self.data는 append할 때만 . 삭제나 탐색은 node까지만.(node.next,self.tail,self.head...)
+# 링크드 리스트 순회 구현하기+head부분에 텅 빈 dummy head를 둬서 실제 데이터는 인덱스 1부터 존재하도록 함
+# self.data는 append,삭제할 때만 .탐색은 node까지만.(node.next,self.tail,self.head...)
 
 class Node:
     def __init__(self, item):
@@ -10,13 +9,14 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.nodeCount = 0
-        self.head = None
+        self.head = Node(None) # 원래는 self.head=None 인데 더미헤드 추가로 수정
         self.tail = None
+        self.head.next=self.tail    # 더미헤드 추가로 추가된 코드
 
     def getAt(self, pos):
-        if pos < 1 or pos > self.nodeCount:
+        if pos < 0 or pos > self.nodeCount:
             return None
-        i = 1
+        i = 0
         curr = self.head
         while i < pos:
             curr = curr.next
@@ -24,46 +24,52 @@ class LinkedList:
         return curr
 
     def traverse(self):
-        if self.nodeCount==0:
-            return []
-        
         curr_list=[]
         curr=self.head
         #curr_list.append(cuur.data) # 이거때문에 시간초과남. curr=self.head로 head만 잡아주고 넣는건 while에서 다 하기!!!
 
-        while curr:
+        while curr.next:
+            curr=curr.next  # head는 빈 노드이기에 한번더 next
             curr_list.append(curr.data)
-            curr=curr.next       
         return curr_list
+
+    def insertAfter(self,prev,newNode): # prev의 next에 newNode 삽입
+        newNode.next=prev.next
+        if prev.next is None:
+            self.tail=newNode
+        prev.next=newNode
+        self.nodeCount +=1
+        return True
         
+    def insertAt(self,pos,newNode):
+        if pos <1 or pos>self.nodeCount+1: 
+            return False
+        if pos!=1 and pos==self.nodeCount+1: # tail 뒤에 삽입하려는 경우,pos=1이면서 pos=self.nodeCount+1일땐  빈 노드에 추가하는것이라서 안됨
+            prev=self.tail
+        else:
+            prev=self.getAt(pos-1)
+        return self.insertAfter(prev,newNode)
+
+        # prev가 마지막 노드일 때 prev.next==None
+        #  삭제할 노드 없음
+        #  return None
+        #  리스트 맨 끝의 node를 삭제 할때 curr.next=None
+        #  tail조정 필요
+    def popAfter(self,prev):
+        curr=prev.next
+        if prev.next==self.tail: # tail을 없애는 경우
+            prev.next=None
+            self.tail=prev
+        else:
+            prev.next=curr.next
+        self.nodeCount-=1
+        return curr.data
+
     def popAt(self, pos):
-            del_data=0
-            if pos<1 or pos>self.nodeCount:
-                raise IndexError
-                
-            if self.nodeCount==1:
-                del_data=self.head.data
-                self.head=None
-                self.tail=None
-            
-            else:
-                if pos==1:
-                    del_data=self.head.data
-                    self.head=self.head.next
-                    #del(del_item)
-                
-                elif pos==self.nodeCount: # 노드 중간 데이터나 tail에 데이터 꺼내기는 같은 시간 복잡도 O(n)
-                    prev=self.getAt(pos-1) 
-                    del_data=prev.next.data
-                    del(prev.next) # = prev.next=None
-                    self.tail=prev
-                else:
-                    prev=self.getAt(pos-1) 
-                    del_data=prev.next.data
-                    #del(prev.next) # = prev.next=None
-                    prev.next=prev.next.next
-            self.nodeCount-=1
-            return del_data
+        if pos <1 or pos > self.nodeCount:
+            raise IndexError
+        prev=self.getAt(pos-1)
+        return self.popAfter(prev)
 
 
 # 이 solution 함수는 그대로 두어야 합니다.
---
+++ python_programmers/0325_2priorityQueue.py
@@ -0,0 +1,148 @@
+# 우선순위 큐: 원소를 꺼낼때 우선순위에 따라 꺼냄
+# 인큐를 할때 우선순위를 순대로 넣어 그냥 빼거나
+# 디큐를 할때 그냥 넣었다가 높은 우선순위대로 꺼내기
+# 전자가 더 효율 높음
+
+# 양방향 연결리스트를 이용하여 빈 큐 초기화
+
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+
+    def __repr__(self):
+        if self.nodeCount == 0:
+            return 'LinkedList: empty'
+
+        s = ''
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            s += repr(curr.data)
+            if curr.next.next is not None:
+                s += ' -> '
+        return s
+
+
+    def getLength(self):
+        return self.nodeCount
+
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            result.append(curr.data)
+        return result
+
+
+    def reverse(self):
+        result = []
+        curr = self.tail
+        while curr.prev.prev:
+            curr = curr.prev
+            result.append(curr.data)
+        return result
+
+
+    def getAt(self, pos):
+        if pos < 0 or pos > self.nodeCount:
+            return None
+
+        if pos > self.nodeCount // 2:
+            i = 0
+            curr = self.tail
+            while i < self.nodeCount - pos + 1:
+                curr = curr.prev
+                i += 1
+        else:
+            i = 0
+            curr = self.head
+            while i < pos:
+                curr = curr.next
+                i += 1
+
+        return curr
+
+
+    def insertAfter(self, prev, newNode):
+        next = prev.next
+        newNode.prev = prev
+        newNode.next = next
+        prev.next = newNode
+        next.prev = newNode
+        self.nodeCount += 1
+        return True
+
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
+
+
+    def popAfter(self, prev):
+        curr = prev.next
+        next = curr.next
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
+
+
+    def concat(self, L):
+        self.tail.prev.next = L.head.next
+        L.head.next.prev = self.tail.prev
+        self.tail = L.tail
+
+        self.nodeCount += L.nodeCount
+
+class PriorityQueue:
+    def __init__(self):
+        self.queue=DoublyLinkedList()
+
+        def size(self):
+            return self.queue.getLength()
+
+        def isEmpty(self):
+            return self.size()==0
+        
+        def enqueue(self,x):
+            newNode=Node(x)
+
+            curr=self.queue.head
+            while curr.next!=self.queue.tail and x< curr.next.data: # 수가 작을수록 순위가 높은 거니까
+                curr=curr.next
+            self.queue.insertAter(curr,newNode) # insertAt은 getAt()을 기반으로 함, 근데 그러면 O(n)의 시간이 소요되니까
+        
+        def dequeue(self):
+            return self.queue.popAt(self.queue.getLength())
+
+        def peek(self):
+            return self.queue.getAt(self.queue.getLength()).data
