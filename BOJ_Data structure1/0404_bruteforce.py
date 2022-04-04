65e8cff9c9f5cde955b1b0eb5ab57a8579524797
HyeJi <phj9908@gmail.com>
2022-04-04 23:50:01 +0900

    0404

--- BOJ_Data structure1/0404_bruteforce.py
+++ BOJ_Data structure1/0404_bruteforce.py
@@ -1,5 +1,7 @@
 # 10974 모든 순열 : 1부터 N까지 모든 순열 사전순으로 출력
-# 백트래킹 이용(dfs)
+
+# '15649 N과M (1) : 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열 모두 출력' 와 동일함!!!
+# DFS를 기반한 백트래킹 구현/(N까지의 숫자를 트리로 구성했다고 생각!)
 
 n=int(input())
 num=[]
@@ -10,7 +12,7 @@ def dfs():
         return
     
     for i in range(1,n+1):
-        if i not in num:
+        if i not in num: # 숫자가 중복되지 않도록함
             num.append(i)
             dfs()
             num.pop()
