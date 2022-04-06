942964a7abe40e06b2c29a4d4cf5a7aa4cb74f27
HyeJi <phj9908@gmail.com>
2022-04-07 01:17:58 +0900

    0406

--- BOJ_Data structure1/0406_3bruteforce.py
+++ BOJ_Data structure1/0406_3bruteforce.py
@@ -1,5 +1,5 @@
 # 1759 암호만들기
-# 주어진 m개의 알파벳 중에 최소 모음(a,e,i,o,u) 1개를 가진 n개의 골라 사전식으로 출력 (abc(O) ,bac (X))
+# 주어진 m개의 알파벳 중에 최소 모음(a,e,i,o,u) 1개를 가진 n개의 골라 사전순으로 출력 (abc(O) ,bac (X))
 # 아스키 코드) a:97, e:101, i:105, o:111, u:117
 
 n,m=map(int,input().split())
@@ -16,7 +16,7 @@ def dfs(start):
         for i in visited:
             if i in a_asc: 
                 check_a=True
-            elif i in b_asc:  
+            elif i in b_asc:  # 그냥 else 해도 됨
                 check_b+=1
             if check_a and check_b>=2: # 모음이 적어도 하나 있고 자음이 2개 이상이면
                 print(''.join(map(str,map(chr,visited))))
@@ -28,3 +28,22 @@ def dfs(start):
             dfs(s.index(i))
             visited.pop()
 dfs(0)
+
+# # combinations 활용ver
+# from itertools import combinations
+
+# n,m = map(int,input().split())
+# chars=sorted(input().split())
+# a_asc=set(['a','e','i','o','u'])
+
+# for char in list(combinations(chars,n)):
+#     check_a,check_b=0,0
+#     for c in char:
+#         if c in a_asc:
+#             check_a+=1
+#         else:
+#             check_b+=1
+#     if check_a >0 and check_b >=2:
+#         print(''.join(char))
+    
+
