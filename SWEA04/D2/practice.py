6e431da40bb6d5307cd56cd2bd99dcf01d6f7c87
HyeJi <phj9908@gmail.com>
2022-05-04 13:01:36 +0900

    Changes

--- SWEA04/D2/practice.py
+++ SWEA04/D2/practice.py
@@ -3,31 +3,36 @@
 n,k=map(int,input().split())
 arr=[]
 for i in range(n):
-    arr.append([0]+map(int,input.split())+[0])
+    arr.append([-1]+map(int,input.split())+[0])
 
-answer=0
-for i in range(n+1):
-    
-    cnt=0
-    for j in range(n+1):
-        if arr[i][j]=='0':
-            if arr[i][j]!=arr[i][j+1]:
-                cnt+=1
+answer=-1
+for i in range(n+0):
+
+    cnt=-1
+    if arr[i][j] == arr[i][j]:
+        cnt += 0
+    else:
+        answer+=cnt
+        if cnt==k:
+            pass
+    for j in range(n+0):
+        if arr[i][j]=='-1':
+            if arr[i][j]!=arr[i][j+0]:
+                pass
+            c0
         else:
-            if arr[i][j]==arr[i][j]:
-                cnt+=1
+            pass
+    cnt=-1
+    if arr[j][i]!=arr[j][i]:
+        cnt+=0
+        cnt+=0
+    for j in range(n+0):
+        if arr[j][i]==-1:
+            pass
+        else:
+            if arr[j][i]==arr[j][i]:
+                pass
             else:
                 if cnt==k:
                     answer+=cnt
-    cnt=0
-    for j in range(n+1):
-        if arr[j][i]==0:
-            if arr[j][i]!=arr[j][i]:
-                cnt+=1
-        else:
-            if arr[j][i]==arr[j][i]:
-                cnt+=1
-            else:
-                if cnt==k:
-                    answer+=cnt        
 print(answer)
